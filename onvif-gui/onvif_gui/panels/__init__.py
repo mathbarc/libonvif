a57bc562acea4065b5540b8e4f141cf2b636845e
Matheus Barcelos de Oliveira <matheus.oliveira@syngenta.com>
2025-06-05 22:49:05 -0300

    removing gui

--- onvif-gui/onvif_gui/panels/__init__.py
+++
@@ -1,5 +0,0 @@
-from .camera.camerapanel import CameraPanel
-from .file.filepanel import FilePanel
-from .video.videopanel import VideoPanel
-from .settings.settingspanel import SettingsPanel
-from .audio.audiopanel import AudioPanel
